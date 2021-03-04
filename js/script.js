be422df87c57cb0f6b10aa78fc97443bd72ddb97
NProlic <nikola.prolic@gmail.com>
2021-03-04 18:21:59 +0100

    small edit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,29 @@ function makeBig() {
     console.log('makeBig() detected a click')
     myVideo.width = 1000;
 }
+let myVideo = document.getElementById("myVideo");
+// preload
+myVideo.controls = false;
 
-// eventListener for make big
-big_btn.addEventListener('click', makeBig)
+function playPause() {
+
+  if (myVideo.paused) {
+    myVideo.play();
+    myVideo.controls = false;
+      }
+  else {
+    myVideo.pause();
+    myVideo.controls = true;
+    }
+}
+// muting sound
+function shutUpPlease() {
+    document.getElementById("myVideo").muted = true;
+    myVideo.muted = true;
+}
+// "unmuting" sound
+function orangeAmp() {
+    // ... logic here: write some code that will mute the video ...
+    document.getElementById("myVideo").muted = true;
+    myVideo.muted = false;
+}
