c3944c6060d78c9eeefd3950a7a45c1ac62c80ec
NProlic <nikola.prolic@gmail.com>
2021-03-05 08:29:19 +0100

    Final version

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 // in js/js.js
-function makeBig() {
-    console.log('makeBig() detected a click')
-    myVideo.width = 1000;
-}
-let myVideo = document.getElementById("myVideo");
-// preload
-myVideo.controls = false;
+/*jshint es5: true */
 
-function playPause() {
+/*The list*/
+let myList = [
+    "Adobe Photoshop",
+    "Adobe XD",
+    "Adobe Illustrator"
+]
 
-  if (myVideo.paused) {
-    myVideo.play();
-    myVideo.controls = false;
-      }
-  else {
-    myVideo.pause();
-    myVideo.controls = true;
-    }
+/*loop to display array of strings*/
+for (let i = 0; i < 3; i++) {
+    content.innerHTML += '<pre>        </pre><u>' + myList[i] + '</u><pre>        </pre>';
 }
-// muting sound
-function shutUpPlease() {
-    document.getElementById("myVideo").muted = true;
+
+/*Preloads video*/
+var vid = document.getElementById("myVideo");;
+
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+  vid.pause();
+}
+
+mute.addEventListener('click',
+  function(){
     myVideo.muted = true;
-}
-// "unmuting" sound
-function orangeAmp() {
-    // ... logic here: write some code that will mute the video ...
-    document.getElementById("myVideo").muted = true;
+  });
+
+unmute.addEventListener('click',
+  function(){
     myVideo.muted = false;
-}
+  });
